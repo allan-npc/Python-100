10151175fc232b140c32a96f397f5e9b75906abc
Allan <contas.allan@gmail.com>
2020-06-25 17:27:25 -0300

    Aula 12 - Módulos e Pacotes

--- Exercícios/Aula 22/Ex.111/utilidadescev/dados/__init__.py
+++
@@ -1,9 +0,0 @@
-def leiaDinheiro(valor):
-    ok = False
-    while not ok:
-        msg = input(valor)
-        if msg.isalpha():
-            print(f'ERRO: \"{msg}\" é um valor inválido')
-        else:
-            ok = True
-            return float(msg.replace(',','.'))
---
+++ Exercícios/Aula 22/Ex.112/Ex.112.py
@@ -0,0 +1,4 @@
+from utilidadescev import moedas
+from utilidadescev import dados
+p = dados.leiaDinheiro('Digite o preço: R$ ')
+moedas.resumo(p,80,35)
